d1805492cba7fc95b9f74568c1936d0c5db207fc
DELEGLISE Leonie <Leonie.Deleglise@bordeaux-inp.fr>
2024-10-16 09:09:39 +0200

    bouton + led ok

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 /*
- * Copyright (c) 2017-2020 Arm Limited and affiliates.
+ * Copyright (c) 2006-2020 Arm Limited and affiliates.
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include "mbed.h"
 
-InterruptIn button(PH_1);
-DigitalOut led(LED1);
-
-
-void flip()
-{
-    led = !led;
-}
+DigitalIn  mypin(PH_1); 
+DigitalOut myled(LED1);
 
 int main()
 {
-    
-    button.rise(&flip);  
+    // check mypin object is initialized and connected to a pin
+    if (mypin.is_connected()) {
+        printf("mypin is connected and initialized! \n\r");
+    }
+    mypin.mode(PullNone);
+
+    // press the button and see the console / led change
+    while (1) {
+        printf("mypin has value : %d \n\r", mypin.read());
+        myled = mypin;
+    }
 }
