2e9047c19fb90d875ee9bd83ee7526450231c4a4
DELEGLISE Leonie <Leonie.Deleglise@bordeaux-inp.fr>
2024-10-16 10:01:58 +0200

    LED + INTERUPT + TIMER

--- main.cpp
+++ main.cpp
@@ -5,20 +5,55 @@
 
 #include "mbed.h"
 
-DigitalIn  mypin(PH_1); 
+DigitalIn mypin(PH_1);
 DigitalOut myled(LED1);
 
-int main()
-{
-    // check mypin object is initialized and connected to a pin
+InterruptIn mypin_irq(PH_1);
+
+bool button_rise_btn = false;
+bool button_fall_btn = false;
+
+Timer t;
+using namespace std::chrono;
+
+void button_rise() {
+    myled =! myled;
+    button_rise_btn= true; 
+    t.start();
+}
+
+void button_fall() {
+    myled =! myled;
+    button_fall_btn = true; 
+    t.stop();
+}
+
+int main() {
     if (mypin.is_connected()) {
         printf("mypin is connected and initialized! \n\r");
     }
+
     mypin.mode(PullNone);
 
-    // press the button and see the console / led change
+    mypin_irq.rise(&button_rise);
+    mypin_irq.fall(&button_fall);
+
     while (1) {
-        printf("mypin has value : %d \n\r", mypin.read());
-        myled = mypin;
+
+        if (button_rise_btn == true) {
+            printf("mypin has value : 1\n\r");
+            button_rise_btn = false; 
+        }
+
+        if (button_fall_btn == true) {
+            printf("mypin has value : 0\n\r");
+            printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
+            button_fall_btn = false; 
+            t == 0;
+        }
+
+        wait_us(100000);
     }
 }
+
+
