d1f7ac03f3f6121d88fa04b46bec2f8020db1474
DELEGLISE Leonie <Leonie.Deleglise@bordeaux-inp.fr>
2024-10-16 10:28:25 +0200

    fin du TP2 - WORKING

--- main.cpp
+++ main.cpp
@@ -9,23 +9,35 @@ DigitalIn mypin(PH_1);
 DigitalOut myled(LED1);
 
 InterruptIn mypin_irq(PH_1);
+Ticker ticker;
 
 bool button_rise_btn = false;
 bool button_fall_btn = false;
+
+int freq_led = 16; 
 
 Timer t;
 using namespace std::chrono;
 
 void button_rise() {
-    myled =! myled;
-    button_rise_btn= true; 
+    button_rise_btn = true;
+    freq_led = freq_led * 1.5; 
     t.start();
 }
 
 void button_fall() {
-    myled =! myled;
-    button_fall_btn = true; 
+    button_fall_btn = true;
     t.stop();
+    ticker.detach(); 
+}
+
+void blink_led() {
+    myled = !myled; 
+}
+
+void update_ticker() {
+    ticker.detach(); 
+    ticker.attach(&blink_led, 1.0 / freq_led); 
 }
 
 int main() {
@@ -40,20 +52,19 @@ int main() {
 
     while (1) {
 
-        if (button_rise_btn == true) {
-            printf("mypin has value : 1\n\r");
+        ticker.attach(&blink_led, 1.0 / freq_led); 
+
+        if (button_rise_btn) {
             button_rise_btn = false; 
+            update_ticker(); 
+            printf("Frequency value: %d Hz\n\r", freq_led);
         }
 
-        if (button_fall_btn == true) {
-            printf("mypin has value : 0\n\r");
+        if (button_fall_btn) {
             printf("The time taken was %llu milliseconds\n", duration_cast<milliseconds>(t.elapsed_time()).count());
             button_fall_btn = false; 
-            t == 0;
+            t.reset(); 
         }
-
-        wait_us(100000);
+        wait_us(100000); 
     }
 }
-
-
