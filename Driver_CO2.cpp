5be7f3ef14d28a79347ec75e840a49af8b106428
Bastien Tabardel <bastien.tabardel@proton.me>
2024-11-13 10:31:23 +0100

    Library for Driver Co2

--- Driver_CO2.cpp
+++ Driver_CO2.cpp
@@ -3,33 +3,15 @@
 #include "PinNames.h"
 using namespace std::chrono;
 
-Driver_CO2::Driver_CO2(PinName SDA, PinName SCL): i2c(SDA,SCL)
+Driver_CO2::Driver_CO2(I2C & i2c_ref ) :i2c(i2c_ref)
 {
-    printf("test drv\n\r");
-    i2c.frequency(100000);
-    uint8_t buf[2] = {0xbe, 0xef};
-    printf("crc 0xbeef : %x\n\r", generate_crc(buf, 2));
-
-    send_c02_command(0x21b1);//start measurments
-    ThisThread::sleep_for(seconds(6));
-
-    uint8_t measures[7];
-    int res = read_c02_command(0xec05, measures, 6, milliseconds(2)); //read_measurment
-
-    printf("Res = %d\r\n", res);
-    printf("Value = \r\n");
-    for (int i =0; i <6; i++)
-    {
-        printf("0x%x\r\n", measures[i]);
-    }
-
-
-    
+    factory_reset();
 }
 
 Driver_CO2::~Driver_CO2()
 {
 }
+
 
 int Driver_CO2::send_c02_command(uint16_t address)
 {
@@ -38,30 +20,21 @@ int Driver_CO2::send_c02_command(uint16_t address)
     buf[1] = address & 0xFF;
     return i2c.write(i2c_addr_co2, (char*)buf, sizeof(buf));
 }
+
 int Driver_CO2::read_c02_command(uint16_t address, uint8_t *value, uint8_t lenght_byte_of_data, milliseconds delay)
 {
     int res;
     static uint8_t buf[2];
     buf[0] = address >> 8;
     buf[1] = address & 0xFF;
-
-    res = i2c.write(i2c_addr_co2, (char*)buf, sizeof(buf));
+    res = i2c.write(i2c_addr_co2, (char*)buf, sizeof(buf));//we send the command address
     if (res != 0)
     {
         return res;
     }
-    ThisThread::sleep_for(delay);
+    ThisThread::sleep_for(delay);//sleep for a minimum time defined in the datasheet
 
-    for (int i=0; i < lenght_byte_of_data; i+=2)
-    {
-        res = i2c.read(i2c_addr_co2, (char*)&value[i], 3);
-        if (res != 0)
-        {
-            return res;
-        }
-
-    }
-    return i2c.read(i2c_addr_co2, (char*)value, 3);
+    return i2c.read(i2c_addr_co2, (char*)value, lenght_byte_of_data);//we read the 2 bytes + CRC
 }
 
 int Driver_CO2::write_co2_command(uint16_t address, uint8_t *value)
@@ -97,4 +70,43 @@ uint8_t Driver_CO2::generate_crc(const uint8_t *data, uint16_t count)
         }
     }
     return crc;
+}
+
+int Driver_CO2::read_co2_measurement(uint16_t * co2_ppm, uint16_t * temperature, uint16_t * RH)
+{
+    uint8_t measure[9];
+    uint8_t crc;
+    
+    read_c02_command(0xec05, measure, 9, milliseconds(2)); //read_measurments
+
+    for (uint8_t i =0; i< 9; i+=3)
+    {
+        crc = generate_crc(&measure[i], 2);// generate the crc of the data
+        if (crc != measure[i +2])//check that it corresponds to the CRC sent by the sensor
+        {
+            printf("CRC_Error \r\n");
+            return 1;
+        }
+    }
+    *co2_ppm = (measure[0] << 8 ) | measure[1];
+    *temperature = (measure[3] << 8 ) | measure[4];
+    *RH = (measure[6] << 8 ) | measure[7];
+    return 0;
+
+}
+
+int Driver_CO2::start_periodic_measurement()
+{
+    return send_c02_command(0x21b1);//start measurments
+}
+
+
+int Driver_CO2::factory_reset()
+{
+    return send_c02_command(0x3632);
+}
+
+int Driver_CO2::stop_periodic_measurement()
+{
+    return send_c02_command(0x3f86);
 }
