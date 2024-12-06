b39f0adb7c8915757cbc7bb542f9f2a394be5542
DELEGLISE Leonie <leoniedeleglise@gmail.com>
2024-12-06 14:54:31 +0100

    last version of main to print on minicom co2

--- main.cpp
+++ main.cpp
@@ -4,32 +4,33 @@
  */
 
 #include "mbed.h"
+#include <stdio.h>
+#include "PinNames.h"
 #include "Driver_CO2.hpp"
+#include <math.h>
 
-int main() {
-    I2C i2c(P1_I2C_SDA, P1_I2C_SCL);
+BufferedSerial serial (USBTX, USBRX);
+using namespace std::chrono;
 
-    Driver_CO2 co2_sensor(i2c);
 
-    if (co2_sensor.start_periodic_measurement() != 0) {
-        printf("Erreur : Impossible de démarrer les mesures périodiques.\r\n");
-        return 1;
+int main()
+{
+    I2C i2c (P1_I2C_SDA, P1_I2C_SCL);
+    i2c.frequency(100000);//set i2c to 100 khz
+    uint16_t co2_ppm, temperature,RH;
+    serial.set_baud(9600);
+    Driver_CO2 drv(i2c);
+    ThisThread::sleep_for(seconds(6));
+    drv.start_periodic_measurement();
+    while(1)
+    {
+        int res = drv.read_co2_measurement(&co2_ppm, &temperature, &RH);
+        printf("Res = %d\r\n", res);
+        printf("CO2 : %d ppm\n\r", co2_ppm );
+        printf("Temperature : %d °C\r\n", (int)floorf(-45 + temperature *175.0 / 65535.0));
+        printf("RH %d % \r\n",(int)floorf( RH * 100.0 / 65535.0));
+        ThisThread::sleep_for(seconds(5));//sleep for a minimum time defined in the datasheet
+
     }
 
-    uint16_t co2_ppm;
-    uint16_t temperature;
-    uint16_t humidity;
-
-
-    while (true) {
-
-        if (co2_sensor.read_co2_measurement(&co2_ppm, &temperature, &humidity) == 0) {
-            printf("CO2: %u ppm, Température: %u, Humidité: %u\r\n", co2_ppm, temperature, humidity);
-        } else {
-            printf("Erreur : Lecture des mesures échouée.\r\n");
-        }
-        ThisThread::sleep_for(1s);
-    }
-
-    return 0;
 }
